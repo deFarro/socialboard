4086a8f22f0ea40f153c5185312f38c6f2eb4b4a
Nikita Svetlov <svetlov.n@gmail.com>
2017-08-31 15:28:14 +0300

    Social panel added to form

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -10,6 +10,14 @@ const Form = ({handleSubmit}) => {
   return (
     <div className="form">
       <form onSubmit={handleSubmit}>
+        <div className="socialChoice">
+          <input type="radio" value="twitter" defaultChecked= "true" name="radio" id="twitter" />
+          <label htmlFor="twitter"><i className="fa fa-twitter" aria-hidden="true"></i></label>
+          <input type="radio" value="facebook" name="radio"  id="facebook" />
+          <label htmlFor="facebook"><i className="fa fa-facebook" aria-hidden="true"></i></label>
+          <input type="radio" value="instagram" name="radio"  id="instagram" />
+          <label htmlFor="instagram"><i className="fa fa-instagram" aria-hidden="true"></i></label>
+        </div>
         <input type="text" pattern="\d*" title="Only digits allowed." placeholder="enter user ID"></input>
         <button type="submit">ADD USER</button>
       </form>
